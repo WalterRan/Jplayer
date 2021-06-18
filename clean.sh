b15c751286b576e48a477cf375b1f6317c633bad
WalterRan <jingran84@163.com>
2021-06-18 11:30:06 +0800

    引入了tox

---
+++ clean.sh
@@ -0,0 +1,11 @@
+rm sparrow_player.egg-info -rf
+rm build -rf
+rm ChangeLog -rf
+rm dist -rf
+rm .tox -rf
+rm list -rf
+
+find . -type d -name '__pycache__' | xargs rm -rf
+find . -name '*.pyc' | xargs rm -rf
+
+tree
